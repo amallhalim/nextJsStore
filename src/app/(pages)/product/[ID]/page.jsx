2235099f563387b8170e36e0e31f8a5cf50c3cab
Amal_Halim <amal@zetaon.com>
2025-05-21 10:22:56 +0300

    Add Suspense for loading state in product details page

--- src/app/(pages)/product/[ID]/page.jsx
+++ src/app/(pages)/product/[ID]/page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import fetchSinglePost from '../../../utils/fetchSinglePost'
 import PostDetails  from '../[ID]/PostDetails'
 
@@ -14,7 +14,9 @@ const data = await fetchSinglePost({id:params?.ID})
   return (
       <div className="max-w-3xl mx-auto p-6 mt-10 bg-white rounded-2xl shadow-lg">
       <h1 className="text-3xl font-bold mb-4 text-indigo-600">📦 Product Details</h1>
-     <PostDetails  data={data} /> 
+         <Suspense fallback={<div>Loading post details...</div>}>
+        <PostDetails data={data} />
+      </Suspense>
     </div>
   )
 }
