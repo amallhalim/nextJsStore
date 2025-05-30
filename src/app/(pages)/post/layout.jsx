f568fb1fbc986f14942734049d0d9dbef8ebd033
Amal_Halim <amal@zetaon.com>
2025-05-30 18:23:40 +0300

    Enhance layout styling and improve Not Found page details

--- src/app/(pages)/post/layout.jsx
+++ src/app/(pages)/post/layout.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function layout({children}) {
   return (
 <>
-    <div style={{backgroundColor:"yellow",height :"100%"}}>
+    <div style={{backgroundColor:"yellow",height :"100%" ,padding:"20px" ,margin:"20px"}}>
 {children}
-        layout
+        layout of posts
         </div>
 
 </>
--- src/app/(pages)/post/not-found.jsx
+++ src/app/(pages)/post/not-found.jsx
@@ -1,21 +1,27 @@
+"use client";
 import Link from 'next/link';
-import React from 'react';
+import { usePathname } from 'next/navigation';
 
 export default function NotFound() {
+  const path = usePathname();
+  const postID = path.split("/")[2]
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4 text-center">
       <h1 className="text-6xl font-bold text-blue-600 mb-4">404</h1>
-      <h2 className="text-2xl font-semibold mb-2">Sorry Not Found postDetails</h2>
+      <h1 className="text-1xl font-bold text-gray-600 mb-4">Sorry Not Found postDetails for
+        <span className="text-1xl font-bold text-blue-600 mb-4">     {postID}</span>
+      </h1>
+
       <p className="text-gray-600 mb-6">
         Sorry, the page you’re looking for doesn’t exist or has been moved.
       </p>
       <Link
-        href="/post"    
-        className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition"    
+        href="/post"
+        className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition"
       >
         Go all post
       </Link>
-   
+
     </div>
   );
 }
