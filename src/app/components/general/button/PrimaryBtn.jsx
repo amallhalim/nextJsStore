fc9badf3591ee7d6c8707a22a10cfdfd005daebc
Amal_Halim <amal@zetaon.com>
2025-06-09 12:28:31 +0300

    Add login, signUp, and mainBtn components; initialize basic structure for authentication pages

---
+++ src/app/components/general/button/PrimaryBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PrimaryBtn() {
+  return (
+    <div>  <button className='bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition'
+ 
+    >mainBtn</button></div>
+  )
+}
