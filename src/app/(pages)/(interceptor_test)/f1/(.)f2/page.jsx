ca6eaea7668eca4342e80f56614a59bc3d7014d5
Amal_Halim <amal@zetaon.com>
2025-06-19 18:55:06 +0300

    feat: add interceptor test pages with navigation links and layout enhancements

---
+++ src/app/(pages)/(interceptor_test)/f1/(.)f2/page.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='bg-red-900 '>
+        <div> interceptors -(.f2)</div>
+        <Link href="/f1"> go to f1</Link>
+    </div>
+  )
+}
---
+++ src/app/(pages)/(interceptor_test)/f1/(.)f3/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>intercepror page</div>
+  )
+}
