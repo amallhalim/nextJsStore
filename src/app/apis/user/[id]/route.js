92c27fa47f0e3403e7ef6afa00d9d721b1fd33c3
Amal_Halim <amal@zetaon.com>
2025-06-21 18:53:01 +0300

    feat: add PATCH and DELETE API methods for user data management

--- src/app/apis/user/[id]/route.js
+++ src/app/apis/user/[id]/route.js
@@ -7,3 +7,27 @@ export async function GET(request,{params}) {
 return  Response.json(user)
 
 }
+export async function PATCH(request,{params}) {
+    const id =  await params.id;
+    console.log("Received ID for PATCH:", id);
+    const body = await request.json();
+    console.log("Received data for PATCH:", body);
+    
+    // Here you can process the data, e.g., partially update it in a database
+    // For now, we'll just return the received data as a response
+    return new Response(JSON.stringify({ message: "Data patched successfully", data: body }), {
+        headers: { "Content-Type": "application/json" },
+    }); 
+    
+}
+export async function DELETE(request,{params}) {
+    const id =  await params.id;
+    console.log("Received data for DELETE:", id);
+    
+    // Here you can process the data, e.g., delete it from a database
+    // For now, we'll just return a success message
+    return new Response(JSON.stringify({ message: "Data deleted successfully" }), {
+        headers: { "Content-Type": "application/json" },
+    });
+    
+}
